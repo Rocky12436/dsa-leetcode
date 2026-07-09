ef1bec9ec9bd945ec6e2776b5d06de977a4e45f1
qaziubedrehaman <96972386+Rocky12436@users.noreply.github.com>
2026-07-09 20:31:50 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (48.30%) - LeetSync

---
+++ 57-insert-interval/insert-interval.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& a, vector<int>& b) {
+        a.insert(a.begin(),b);
+        sort(a.begin(),a.end());
+        int start1=a[0][0];
+        int end1 = a[0][1];
+        vector<vector<int>>res;
+        for(int i =1 ;i<a.size();i++)
+        {
+            int start2=a[i][0];
+            int end2=a[i][1];
+            if(end1>=start2)
+            {
+                start1=start1;
+                end1=max(end1,end2);
+                continue;
+            }
+            res.push_back({start1,end1});
+            start1=start2;
+            end1=end2;
+            
+        }
+        res.push_back({start1,end1});
+        return res;
+    }
+};
