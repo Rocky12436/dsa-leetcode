d16b8060128bf46f338b42d1773b88dd76db8c9a
qaziubedrehaman <96972386+Rocky12436@users.noreply.github.com>
2026-07-13 22:24:31 +0530

    Time: 3 ms (73.99%) | Memory: 29.1 MB (12.31%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& arr) {
+        int n = arr.size();
+    vector<int> res(n);
+    stack<int> st;
+    for(int  i  = n-2;i>=0;i--)
+    {
+        st.push(arr[i]);
+    }
+
+    // Traverse from right to left
+    for(int i = n - 1; i >= 0; i--) {
+        
+        // Remove all smaller or equal elements
+        while(!st.empty() && st.top() <= arr[i]) {
+            st.pop();
+        }
+
+        // If stack is empty → no greater element
+        if(st.empty()) res[i] = -1;
+        else res[i] = st.top();
+
+        // Push current element
+        st.push(arr[i]);
+    }
+
+    return res;
+    }
+};
